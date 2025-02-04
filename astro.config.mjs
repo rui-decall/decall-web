4902f32b3978bccef08027cb2085e888d61da9ea
JunyaoC <junyaoxiandingchan@gmail.com>
2025-02-04 15:48:51 +0800

    Add Tailwind CSS configuration and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,14 @@ import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 
 import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   output: 'server',
   integrations: [react(), vue(), tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
